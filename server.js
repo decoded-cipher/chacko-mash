f91046b91decd970e84dcda678807fcfc39bf68e
Arjun Krishna <arjun12345krishna@gmail.com>
2021-06-06 05:06:28 +0530

    Welcome Message

--- server.js
+++ server.js
@@ -17,10 +17,26 @@ client.on('message', (message) => {
 
         // console.log(CMD_NAME);
         // console.log(args);
-        
+
         if (CMD_NAME === 'wakeup') {
             message.channel.send('Whats Up, Mahn...')
             message.reply('Bye, Let me take a Nap...')
         }
     }
+})
+
+
+client.on('guildMemberAdd', guildMember => {
+
+    var welcomeImages = [
+        "http://www.whykol.com/wp-content/uploads/2015/04/godfather-051.jpg",
+        "http://www.whykol.com/wp-content/uploads/2015/04/godfather-050.jpg",
+        "https://commentphotos.com/images/opengraph/CommentPhotos.com_1392031418.jpg",
+        "http://www.whykol.com/wp-content/uploads/2014/11/punjabi-house-163.jpg"
+    ]
+    var randomIndex = Math.floor(Math.random() * welcomeImages.length);
+
+    guildMember.guild.channels.cache.get('849255437228572703').send(`Welcome <@${guildMember.user.id}> to Inovus Labs IEDC Discord Server!\n`, {
+        files: [`${welcomeImages[randomIndex]}`]
+    })
 })
