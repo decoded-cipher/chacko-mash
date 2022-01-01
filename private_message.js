cafdbb9f43b289c7afd22dcf1841288546ee9c44
Arjun Krishna <arjun12345krishna@gmail.com>
2022-01-01 13:23:54 +0530

    Just some edits

--- private_message.js
+++ private_message.js
@@ -3,13 +3,18 @@ module.exports = {
     // description : "",
     execute(message, Discord) {
         var newEmbed = new Discord.MessageEmbed()
-        .setColor('#4b9fc3')
-        .setTitle('Onam Wishes from Inovus Labs IEDC')
-        .setURL('https://inovus-labs.web.app')
-        .setDescription('Wishing you a life as Colourful as Pookalam, as festive as this Onam Festival, and as prosperous as the Bountiful Harvest. \n**Happy Onam to you!** \n\nOn the occasion of Onam, I pray that your life is filled with Abundance, Happiness, and Success. \n**Onam greetings to you & your family.**')
-        .setImage('https://cdn.discordapp.com/attachments/844829664405225482/878453861390647296/130306445-3b1a60f6-8d12-418e-91e9-88b9d85f9e6f.jpg')
+        .setColor('#6c9380')
+        .setTitle('Happy New Year | Inovus Labs IEDC')
+        .setURL('https://inovuslabs.org')
+        .setDescription('*“We are opening a new book. Its pages are blank. We are gonna put words on them ourselves. The book is called **Opportunity** & its first chapter is the **New Year Day**”*\n\n> We wish that the coming year brings to you more strength & dedication to achieve all your goals with your commitment & smart work. :dove:\n\nWishing you a **Happy New Year** with the hope that you will have many blessings in the year to come. :sparkles:')
+        .setImage('https://media.discordapp.net/attachments/843216992521158706/926547615506657340/Instagram_post_-_5.png')
         .setFooter('I\'m a bot, don\'t reply to me.')
 
-        message.channel.send(newEmbed)
+        message.guild.members.cache.forEach(member => {
+            member.send(newEmbed)
+                .catch(e => {
+                    console.error(`Couldn't message ${member.user.tag}`)
+                })
+        })
     }
 } 
