7ccb835112263815445a417e5d148423b6dba729
Arjun Krishna <arjun12345krishna@gmail.com>
2022-02-05 19:50:02 +0530

    Birthday Bot Working

--- birthday.js
+++ birthday.js
@@ -11,17 +11,37 @@ module.exports = {
                 
                 var nameInput = bDayData[key]['Full Name']
                 var discordIdInput = DiscordUserData.username + '#' + DiscordUserData.discriminator
-                var AgeWish = "23rd Birthday"
+            
+                var birthYear = new Date(bDayData[key]['Date of Birth']).getFullYear();
                 
                 var avatarImage = `https://cdn.discordapp.com/avatars/${DiscordUserData.id}/${DiscordUserData.avatar}.png`
                 var gender = bDayData[key]['Gender']
                 // var discordAvatar = DiscordUserData.username
             }
         }
-        console.log(nameInput);
-        console.log(discordIdInput);
-        console.log(AgeWish);
-        console.log(avatarImage);
+        
+        var presentYear = new Date().getFullYear();
+        var age = presentYear - birthYear
+        console.log(age);
+
+        switch (age % 10) {
+            case 1 :
+                AgeWish = age + "st Birthday";
+                break;
+            case 2 :
+                AgeWish = age + "nd Birthday";
+                break;
+            case 3 :
+                AgeWish = age + "rd Birthday";
+                break;
+            default :
+                AgeWish = age + "th Birthday";
+        }
+
+        // console.log(nameInput);
+        // console.log(discordIdInput);
+        // console.log(AgeWish);
+        // console.log(avatarImage);
 
         const fs = require('fs')
         const { createCanvas, loadImage } = require('canvas')
@@ -35,30 +55,38 @@ module.exports = {
         var designTemplate = ''
         gender == 'Male' ? designTemplate = 'template_3.jpg' : designTemplate = 'template_2.jpg';
 
-        loadImage(`assets/birthday/templates/${designTemplate}`).then((image) => {
-
-            ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-            ctx.fillStyle = '#000'
-            ctx.textAlign = "center";
-            
-            ctx.font = '48px Lithos Pro Regular, sans-serif'
-            ctx.fillText(nameInput, 425, 870)
-            
-            ctx.font = '25px Lithos Pro Regular, sans-serif'
-            ctx.fillText(discordIdInput, 425, 910)
-
-            ctx.font = '96px Alex Brush, cursive'
-            ctx.fillText(AgeWish, 420, 1190)
+            loadImage(avatarImage).then((avatar) => {
+    
+                loadImage(`assets/birthday/templates/${designTemplate}`).then((image) => {
+                    
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+                    ctx.fillStyle = "#39393B";
+                    ctx.fillRect(264, 472, 323, 323);
+                    ctx.drawImage(avatar, 273, 481, 305, 305)
+                    
+                    ctx.fillStyle = '#000'
+                    ctx.textAlign = "center";
+                    
+                    ctx.font = '48px Lithos Pro Regular, sans-serif'
+                    ctx.fillText(nameInput, 425, 870)
+                    
+                    ctx.font = '25px Lithos Pro Regular, sans-serif'
+                    ctx.fillText(discordIdInput, 425, 910)
         
-            var buffer = canvas.toBuffer('image/png')
-            fs.writeFileSync('assets/birthday/output.png', buffer)
-        })
+                    ctx.font = '96px Alex Brush, cursive'
+                    ctx.fillText(AgeWish, 420, 1190)
+                
+                    var buffer = canvas.toBuffer('image/png')
+                    fs.writeFileSync('assets/birthday/output.png', buffer)
+                })
+                
+            })
 
         client.channels.cache.get(TARGET_CHANNEL).send({
             files: [`assets/birthday/output.png`]
         })
                 
-        console.log(DiscordUserData);
+        // console.log(DiscordUserData);
         // console.log(bDayData);
     }
 }
