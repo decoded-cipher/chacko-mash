32cffc1993a6c414a3833dfe2218493fd2be88a6
Arjun Krishna <arjun12345krishna@gmail.com>
2022-03-25 00:00:20 +0530

    Assign a Role to any no: of Users

---
+++ roles.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name : "/roles",
+    // description : "",
+    execute(client, message, TARGET_ROLE, args) {
+        // console.log(TARGET_ROLE);
+        // console.log(args);
+
+        var role = message.guild.roles.cache.find(r => r.id === TARGET_ROLE);
+        // console.log(role);
+
+        var users = args.split(/[ ]+/);
+        users.forEach(user => {
+            user = user.replace(/[^0-9]/g, '');
+            // console.log(user);
+
+            // user.roles.add(role);
+            message.guild.member(user).roles.add(role);
+        });
+    }
+} 
