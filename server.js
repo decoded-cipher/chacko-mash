5513bcc6e21e7e848e5a30cab707172736f26724
Arjun Krishna <arjun12345krishna@gmail.com>
2022-05-23 03:07:01 +0530

    Minor fix

--- server.js
+++ server.js
@@ -13,19 +13,7 @@ var PREFIX = "$"
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 client.commands = new Discord.Collection()
 
-// ----- Firebase Config Start -----
-var admin = require("firebase-admin");
-var serviceAccount = require("./firebase-admin.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-});
-
-var database = admin.database()
-var memberData = database.ref(process.env.FIREBASE_DATABASE_PATH)
-var birthdayData = database.ref(process.env.FIREBASE_BDAY_PATH)
-// ----- Firebase Config End -----
+// ---------------------------------------------------------------------------------------------------------------------
 
 var api = require('./apiClient.js')
 
@@ -50,7 +38,7 @@ client.commands.set(welcome.name, welcome)
 var dmUser = require("./dmUser")
 client.commands.set(dmUser.name, dmUser)
 
-
+// ---------------------------------------------------------------------------------------------------------------------
 
 client.once('ready', async () => {
     await client.commands.get('/onReady').execute(client)
@@ -73,12 +61,22 @@ client.on('guildMemberAdd', guildMember => {
 })
 
 client.on('message', (message) => {
+
     if (message.guild && message.content.startsWith('/help')) {
         client.commands.get('/helpCommand').execute(message, Discord)
+
+    } else if (message.guild === null && !message.author.bot) {
+
+        var newEmbed = new Discord.MessageEmbed()
+            .setColor('#4b9fc3')
+            .setAuthor(message.author.username, `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`)
+            .setDescription(message.content)
+            .setFooter(IST.toString())
+        client.channels.cache.get(process.env.TARGET_CHANNEL).send(newEmbed);
+
     }
+
 });
-
-
 
 client.on('message', async (message) => {
     if (message.content.startsWith(PREFIX)) {
@@ -89,11 +87,10 @@ client.on('message', async (message) => {
             TARGET_CHANNEL = TARGET_CHANNEL.replace(/[^0-9\s]/g, '')
 
             if (CMD_NAME === 'bot') {
-
                 client.channels.cache.get(TARGET_CHANNEL).send(args);
 
             } else if (CMD_NAME === 'bday') {
-                
+
                 api.getExtUserData(args).then((userData) => {
                     client.commands.get('/birthday').execute(client, TARGET_CHANNEL, userData, IST)
                 }).catch((error) => {
@@ -101,13 +98,11 @@ client.on('message', async (message) => {
                 })
 
             } else if (CMD_NAME === 'role') {
-
                 client.commands.get('/roles').execute(client, message, TARGET_CHANNEL, args)
-                
-            } else if (CMD_NAME === 'dm') {
 
+            } else if (CMD_NAME === 'dm') {
                 client.commands.get('/dmUser').execute(client, message, TARGET_CHANNEL, args)
-                
+
             } else {
                 message.reply('\nI think you are using the commands in an invalid format.\nTo check-out the command formats, try: **/help**')
             }
@@ -115,20 +110,5 @@ client.on('message', async (message) => {
         } else {
             message.reply(`\nYou are not allowed to use this command.\nPlease contact <@&${process.env.PRIORITY_ROLE_01}> for more details.`)
         }
-    }
-})
-
-
-
-client.on('message', (message) => {
-    if (message.guild === null && !message.author.bot) {
-
-        var newEmbed = new Discord.MessageEmbed()
-            .setColor('#4b9fc3')
-            // .setAuthor(message.author.username)
-            .setAuthor(message.author.username, `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`)
-            .setDescription(message.content)
-            .setFooter(IST.toString())
-        client.channels.cache.get(process.env.TARGET_CHANNEL).send(newEmbed);
     }
 })
